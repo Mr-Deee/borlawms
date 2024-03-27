22654fe630a03b6a1e1731f279a9a4e50200436f
Merchant_Daniel <merchantdaniel8@gmail.com>
2024-03-27 12:19:57 +0000

    +googlemaps +add internet permission +add fine and course permission +Add network state

--- lib/pages/Profilepage.dart
+++ lib/pages/Profilepage.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import '../Model/WMSDB.dart';
-
 
 class Profilepg extends StatefulWidget {
   const Profilepg({super.key});
@@ -21,17 +20,17 @@ class _ProfilepgState extends State<Profilepg> {
       ),
       body: ProfileWidget(),
     );
-
   }
 }
 
 class ProfileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    var email = Provider.of<WMS>(context,listen: false).riderInfo?.email??"";
-    var lclientname = Provider.of<WMS>(context,listen: false).riderInfo?.firstname??"";
-    var phoneNumber = Provider.of<WMS>(context,listen: false).riderInfo?.phone??"";
+    var email = Provider.of<WMS>(context, listen: false).riderInfo?.email ?? "";
+    var lclientname =
+        Provider.of<WMS>(context, listen: false).riderInfo?.firstname ?? "";
+    var phoneNumber =
+        Provider.of<WMS>(context, listen: false).riderInfo?.phone ?? "";
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -41,20 +40,52 @@ class ProfileWidget extends StatelessWidget {
             height: 120, // Adjust the height as needed
             child: Image.asset(
               'assets/images/wms.png',
-            ),),
+            ),
+          ),
         ),
-
         SizedBox(height: 10),
         Text(
           lclientname,
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
-        Text(email,style: TextStyle(fontSize: 19),),
+        Text(
+          email,
+          style: TextStyle(fontSize: 19),
+        ),
         Text(phoneNumber),
-
         SizedBox(height: 20),
-
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: _deleteCurrentUser,
+                child: Text('Delete Current User'),
+              ),
+              SizedBox(height: 20),
+              Text(_deleteMessage),
+            ],
+          ),
+        ),
       ],
     );
+  }
+
+  void _deleteCurrentUser() async {
+    try {
+      // Get current user
+      User? user = _auth.currentUser;
+
+      // Delete the user
+      await user!.delete();
+
+      setState(() {
+        _deleteMessage = 'User deleted successfully.';
+      });
+    } catch (e) {
+      setState(() {
+        _deleteMessage = 'Failed to delete user: $e';
+      });
+    }
   }
 }
