d40bf4850cabd83126ad1d3598ed351618010558
mr-DEEE <merchantdaniel8@gmail.com>
2024-10-06 01:05:10 +0000

    reroughtt

--- lib/configMaps.dart
+++ lib/configMaps.dart
@@ -13,5 +13,4 @@ String title = "";
 double starCounter = 0.0;
 StreamSubscription<Position>? homeTabPageStreamSubscription;
 String rideType = "";
-String serverToken =
-    "key=AAAAVtKD6xg:APA91bFcNoAC4CKFdKqZEU8eNWWvcl_mHtWI12bMDChOUvq7lFTxs5QzDiiInaRwMCgN9YuuQPEgJ64Po4w9GujcG2maNOe18cvFUVavbq31giVxmu0wRGY84iDRd884azPUKkruthhl";
+String serverToken = "key=AAAAVtKD6xg:APA91bFcNoAC4CKFdKqZEU8eNWWvcl_mHtWI12bMDChOUvq7lFTxs5QzDiiInaRwMCgN9YuuQPEgJ64Po4w9GujcG2maNOe18cvFUVavbq31giVxmu0wRGY84iDRd884azPUKkruthhl";
